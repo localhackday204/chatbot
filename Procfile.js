b4d68233e6121b2bb6600e1b1d2328afe17c0e46
Max Gutkin <gutkinm4@myumanitoba.ca>
2016-12-03 14:59:44 -0600

    test

---
+++ Procfile.js
@@ -0,0 +1,2 @@
+// JavaScript source code
+web: node app.js
---
+++ app.js
@@ -0,0 +1,56 @@
+// JavaScript source code
+var restify = require('restify');
+var builder = require('botbuilder')
+//setup restify server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url);
+});
+
+//create chatbot
+var connector = new builder.ChatConnector({
+    appId: process.env.MSappID,
+    appPassword: process.env.MSappPassword
+});
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+var intents = new builder.IntentDialog();
+bot.dialog('/', intents);
+
+//bot dialogues
+intents.matches(/^play music/i, [
+        function (session) {
+            session.send('Stop there, and let me correct it, I wanna live my life from a new perspective.');
+        }
+]);
+intents.matches(/^change name/i, [
+    function (session) {
+        session.beginDialog('/profile');
+    },
+    function (session, results) {
+        session.send('Ok... Changed your name to %s', session.userData.name);
+    }
+]);
+intents.onDefault([
+    function (session, args, next) {
+        if (!session.userData.name) {
+            session.beginDialog('/profile');
+        } else {
+            next();
+        }
+    },
+    function (session, results) {
+        session.send('Hello %s!', session.userData.name);
+    }
+]);
+
+bot.dialog('/profile', [
+    function (session) {
+        builder.Prompts.text(session, 'Hi! What is your name?');
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        session.endDialog();
+    }
+]);
+
